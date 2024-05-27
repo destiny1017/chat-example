8cf586b220f1893447ec9cd40f726ba12345701f
eogh6428@naver.com <destiny1017@nate.com>
2024-05-27 20:45:02 +0900

    Stomp WebSocket

---
+++ src/main/java/websocket/example/stompChat/StompChatApplication.java
@@ -0,0 +1,12 @@
+package websocket.example.stompChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StompChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StompChatApplication.class, args);
+    }
+}
